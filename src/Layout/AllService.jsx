c2cbb640b3faa7f78e82707a01448e7ab532fcae
AkashPaul923 <akashpaul923@gmail.com>
2025-02-12 00:44:25 +0600

    assending order sort add

--- src/Layout/AllService.jsx
+++ src/Layout/AllService.jsx
@@ -21,6 +21,16 @@ const AllService = () => {
 
     },[search])
 
+    
+    const handleAscendingService = () => {
+        setLoading(true)
+        const ascendingService = [...services].sort((a,b) => a.servicePrice - b.servicePrice)
+        setServices(ascendingService)
+        setLoading(false)
+    }
+    
+    
+
     const handleSearch = (e) =>{
         setSearch(e.target.value)
     }
@@ -31,12 +41,19 @@ const AllService = () => {
             <Helmet>
                 <title>All Service || ElectroSavvy</title>
             </Helmet>
-            <label className="input input-bordered flex items-center gap-2 mt-4 max-w-xs md:max-w-lg mx-auto">
-                <input onChange={handleSearch} type="text" className="grow" placeholder="Search" />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
-                    <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
-                </svg>
-            </label>
+            <div className="flex flex-col lg:flex-row justify-between items-center lg:items-end gap-5 max-w-screen-2xl mx-auto ">
+                <label className="input input-bordered flex items-center gap-2 mt-4 w-full max-w-xs md:max-w-lg mx-auto lg:mx-0">
+                    <input onChange={handleSearch} type="text" className="grow" placeholder="Search" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 opacity-70">
+                        <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
+                    </svg>
+                </label>
+                <div>
+                    <button onClick={handleAscendingService} className="btn btn-primary">Price: Low-High</button>
+                    
+                </div>
+            
+            </div>
             <p className="text-center text-4xl font-bold my-7"  data-aos="zoom-in" data-aos-duration="1000">All Services</p>
             {
                 loading && <div className="flex justify-center my-[300px]"><span className="loading loading-bars loading-lg"></span></div>
